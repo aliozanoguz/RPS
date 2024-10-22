5fcade434c96b05fb1d99f73a95795b247c8c2b6
aliozanoguz <ali.oguz@hisarschool.k12.tr>
2024-10-22 15:05:39 +0300

    Initial Commit

---
+++ RPS/Sign.swift
@@ -0,0 +1,18 @@
+enum Sign {
+    case rock
+    case paper
+    case scissors
+    
+    var emoji: String{
+        switch self{
+        case.rock:
+            return"🪨"
+        case.paper:
+            return "🧻"
+        case.scissors:
+            return "✂️"
+            
+        }
+    }
+}
+
